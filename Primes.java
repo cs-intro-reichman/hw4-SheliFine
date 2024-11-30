6b8bec9d8d2016a96cfbe60dac550179f9817cf3
Sheli Fine <sheli.fine@post.runi.ac.il>
2024-11-30 13:49:03 +0200

    First try prime numbers

--- Primes.java
+++ Primes.java
@@ -1,5 +1,35 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int untilNumber = Integer.parseInt(args[0]);
+
+        boolean[] arr = new boolean [untilNumber];
+
+        for (int i = 2; i < untilNumber; i++) {
+            arr[i] = true;
+        }
+        
+        // Changes multifications of prime num to false and skips them.
+     for (int i = 2; i < untilNumber; i++) {
+        if (arr[i] == true){
+            for (int j = 2; i * j < arr.length; j++) {
+                    arr[i * j] = false;
+            }
+    }
+}
+    System.out.println("Prime numbers up to " + untilNumber + ":");
+    
+    int countPrimes = 0;
+    
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] == true){
+        System.out.println(i);
+        countPrimes++;
+             }
+        }
+
+        int percentPrime = (int) (((double) countPrimes/untilNumber) * 100);
+
+    System.out.println("There are " + countPrimes + " primes between 2 and " + untilNumber +
+    " (" + percentPrime + "% are primes)");    
     }
 }
