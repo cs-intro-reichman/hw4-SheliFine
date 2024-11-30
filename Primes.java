455ab05f5c7426a3b9c33e0ca094c515049a7559
Sheli Fine <sheli.fine@post.runi.ac.il>
2024-11-30 14:49:51 +0200

    failed when args[0] was prime, fixed

--- Primes.java
+++ Primes.java
@@ -2,14 +2,14 @@ public class Primes {
     public static void main(String[] args) {
         int untilNumber = Integer.parseInt(args[0]);
 
-        boolean[] arr = new boolean [untilNumber];
+        boolean[] arr = new boolean [untilNumber + 1];
 
-        for (int i = 2; i < untilNumber; i++) {
+        for (int i = 2; i <= untilNumber; i++) {
             arr[i] = true;
         }
         
         // Changes multifications of prime num to false and skips them.
-     for (int i = 2; i < untilNumber; i++) {
+     for (int i = 2; i <= untilNumber; i++) {
         if (arr[i] == true){
             for (int j = 2; i * j < arr.length; j++) {
                     arr[i * j] = false;
@@ -20,7 +20,7 @@ public class Primes {
     
     int countPrimes = 0;
     
-    for (int i = 0; i < arr.length; i++) {
+    for (int i = 2; i < arr.length; i++) {
         if (arr[i] == true){
         System.out.println(i);
         countPrimes++;
