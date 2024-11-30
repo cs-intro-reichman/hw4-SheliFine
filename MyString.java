2782a1c485586037c813cfed881e9068e7743038
Sheli Fine <sheli.fine@post.runi.ac.il>
2024-11-30 21:50:03 +0200

    First try MyString

--- MyString.java
+++ MyString.java
@@ -18,13 +18,38 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        for (int i = 0; i < str.length(); i++) {
+            int ch = str.charAt(i);
+            // checks ASCII num to check if uppercase letter 
+            // and adds 32 to make it lowercase, using ASCII.
+            if (ch > 64 && ch < 91){
+                ch += 32;
+                str = str.substring(0, i) + (char) ch + str.substring(i + 1);
+            } else {
+                continue;
+            }
+        } 
+        return str;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) == str2.charAt(0)){
+                boolean equal = true;
+                for (int j = 1; j < str2.length(); j++) {
+                    if (str1.charAt(i + j) != str2.charAt(j)){
+                        equal = false;
+                        break;
+                    } 
+                }
+            if (equal){
+                    return true;
+            }
+        }
+    }
+
         return false;
     }
 }
